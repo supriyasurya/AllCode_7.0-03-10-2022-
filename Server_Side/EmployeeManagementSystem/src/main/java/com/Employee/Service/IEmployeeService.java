92c4c18e292221eb01d4509e6bd26c7428d2193a
supriyasurya <supriyadhakshi2020@gmail.com>
2022-10-16 23:58:13 -0700

    Employee Management Spring bootApplication

---
+++ Server_Side/EmployeeManagementSystem/src/main/java/com/Employee/Service/IEmployeeService.java
@@ -0,0 +1,18 @@
+package com.Employee.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.Employee.Entity.Employee;
+
+public interface IEmployeeService {
+	
+	Integer saveEmployee(Employee employee);
+	
+	public List<Employee> getAllEmployees();
+	
+	Optional<Employee> getEmployee(Integer id);
+
+	
+	
+}
